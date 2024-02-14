a2562c2dae9400e2368d6c97252edd96d27c2d16
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2024-02-14 20:46:58 +1100

    save

---
+++ Feb-14-2024/2149.rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=2149 lang=cpp
+ *
+ * [2149] Rearrange Array Elements by Sign
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int>v1,v2,ans;
+        
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]>0)v1.push_back(nums[i]);
+            else v2.push_back(nums[i]);
+        }
+        int ind1=0,ind2=0;
+        
+
+        while(ind2<nums.size()/2){
+            ans.push_back(v1[ind1]);
+            ind1++;
+            ans.push_back(v2[ind2]);
+            ind2++;
+        }
+        return ans;
+    }
+};
+
+
+// @lc code=end
+
